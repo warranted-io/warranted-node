46d361f629c11855040e349638a83ffb275ba52b
Winston Howes <winstonhowes@gmail.com>
2024-05-09 11:11:18 -0700

    add more functionality to node lib

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const { createHMAC, timeSafeCompare } = require('./cryptoHelper');
 
 class client {
@@ -15,6 +16,16 @@ class client {
         }
         this.accountId = accountId;
         this.authToken = authToken;
+        this.host = 'https://app.warranted.io';
+        this.headers = {};
+    }
+
+    setHost(host) {
+        this.host = host;
+    }
+
+    setHeaders(headers) {
+        this.headers = headers;
     }
 
     /**
@@ -27,6 +38,103 @@ class client {
     validateRequest(signature, url, body) {
         const hmac = createHMAC(url, body, this.authToken);
         return timeSafeCompare(signature, hmac);
+    }
+
+    decisions = {
+        /**
+         * Get details about a decision
+         * @param {string} decisionId - the id of the decision to fetch
+         * @returns {object} - Details at: http://app.warranted.io/docs/decisions
+         */
+        get: async (decisionId) => {
+            if (!decisionId || !String(decisionId).startsWith('decision-')) {
+                throw new Error('Invalid Decision Id. Must start with `decision-`');
+            }
+            const response = await axios({
+                method: 'get',
+                url: `${host}/api/v1/decisions/${decisionId}`,
+                auth: {
+                    username: this.accountId,
+                    password: this.authToken,
+                },
+                headers: this.headers,
+            });
+            return response.data;
+        },
+    }
+
+    lawEnforcementRequests = {
+        /**
+         * Get either a list of lawEnforcementRequests or one specific request
+         * @param {object | string} options - If an object, it should contain either a `startAt` or `limit` parameter or both.
+         * If a string it should be a `lawEnforcementRequestId`
+         * @returns {array | object} - Details at: http://app.warranted.io/docs/lawEnforcementRequests
+         */
+        get: async (options) => {
+            let url = `${host}/api/v1/lawEnforcementRequests`;
+            if (typeof options === 'string' || options instanceof String) {
+                url += `/${options}`;
+            } else if (typeof options === 'object' && !Array.isArray(options) && options !== null) {
+                const params = new URLSearchParams();
+                if (!isNaN(parseInt(options.startAt, 10))) {
+                    params.append('startAt', String(parseInt(options.startAt, 10)));
+                }
+                if (!isNaN(parseInt(options.limit, 10))) {
+                    params.append('limit', String(parseInt(options.limit, 10)));
+                }
+                url += `?${params.toString()}`;
+            }
+            const response = await axios({
+                method: 'get',
+                url: url,
+                auth: {
+                    username: this.accountId,
+                    password: this.authToken,
+                },
+                headers: this.headers,
+            });
+            return response.data;
+        },
+
+        /**
+         * Update a lawEnforcementRequest
+         * @param {object} lawEnforcementRequest - a law enforcement request object.
+         * @returns {object} - Details at: http://app.warranted.io/docs/lawEnforcementRequests
+         */
+        update: async (lawEnforcementRequest) => {
+            const response = await axios({
+                method: 'put',
+                url: url,
+                auth: {
+                    username: this.accountId,
+                    password: this.authToken,
+                },
+                data: {
+                    lawEnforcementRequest,
+                },
+                headers: Object.assign({'Content-Type': 'application/json'}, this.headers),
+            });
+            return response.data;
+        }
+    }
+
+    me = {
+        /**
+         * Get data about the current user
+         * @returns {object} - Details at: http://app.warranted.io/docs/me
+         */
+        get: async () => {
+            const response = await axios({
+                method: 'get',
+                url: `${host}/api/v1/me`,
+                auth: {
+                    username: this.accountId,
+                    password: this.authToken,
+                },
+                headers: this.headers,
+            });
+            return response.data;
+        },  
     }
 }
 
